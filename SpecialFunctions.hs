c234f7bb71cbd5cdcf048379bd4ca72f9a2dd564
Apollo Hogan <apollo.hogan@gmail.com>
2019-11-11 22:51:53 -0500

    .

--- SpecialFunctions.hs
+++ SpecialFunctions.hs
@@ -52,9 +52,15 @@ sf_expm1 x = kahan_sum $ map snd $ iterate (\(n,t)->(n+1,t*x/(fromIntegral n))) 
 sf_cos :: Value -> Value
 sf_cos x = kahan_sum $ map snd $ iterate (\(n,t)->(n+2,(-t*x*x/(fromIntegral n)/(fromIntegral (n+1))))) (1,1)
 
+sf_cosh = cosh
+
 -- vercosine function
 sf_vcos :: Value -> Value
 sf_vcos x = 2 * (sf_cos $ x/2)^2
+
+-- havercosine function
+sf_hvcos :: Value -> Value
+sf_hvcos x = (sf_cos $ x/2)^2
 
 ----------------------------------------
 -- sin(x)
@@ -62,14 +68,44 @@ sf_vcos x = 2 * (sf_cos $ x/2)^2
 sf_sin :: Value -> Value
 sf_sin x = kahan_sum $ map snd $ iterate (\(n,t)->(n+2,(-t*x*x/(fromIntegral n)/(fromIntegral (n+1))))) (2,x)
 
+sf_sinh = sinh
+
+sf_asin :: Value -> Value
+sf_asin = asin
+
 -- versine function
 sf_vsin :: Value -> Value
 sf_vsin x = 2 * (sf_sin $ x/2)^2
 
+-- haversine function
+sf_hvsin :: Value -> Value
+sf_hvsin x = (sf_sin $ x/2)^2
+
 ----------------------------------------
--- Compute the exsecant function
+-- secant function
+sf_sec x = 1 / (cos x)
+
+-- exsecant function
 sf_exsec x = 2 * (sf_sin $ x/2)^2 * (sf_sec x)
 
+----------------------------------------
+-- tangent function
+sf_tan = tan
+
+-- hyperbolic tangent function
+sf_tanh = tanh
+
+-- cotangent
+sf_cot x = sf_cos(x) / sf_sin(x)
+-- = 1/sf_tan x
+
+
+----------------------------------------
+-- Gudermannian function
+sf_gud x = sf_asin(sf_tanh x)
+-- = 2*sf_atan(sf_exp(x)) - pi/2
+-- = 2*sf_atan(sf_tanh(x/2))
+-- = sf_atan(sf_sinh(x))
 
 ----------------------------------------
 sf_gamma :: Value -> Value
