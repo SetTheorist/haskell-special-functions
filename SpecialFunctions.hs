6f2f89be966aa9113f8a23bba5559b12d625ac2f
Apollo Hogan <apollo.hogan@gmail.com>
2019-11-13 22:58:38 -0500

    Airy Ai Bi (series)

--- SpecialFunctions.hs
+++ SpecialFunctions.hs
@@ -232,6 +232,34 @@ steeds (a1:as) (b0:b1:bs) =
             --in cn:(recur cn delcn dn as bs)
 
 ----------------------------------------
+
+sf_airy_ai z = airy_ai_series z
+sf_airy_bi z = airy_bi_series z
+
+ai0 = 3**(-2/3)/sf_gamma(2/3)
+ai'0 = -3**(-1/3)/sf_gamma(1/3)
+airy_ai_series z =
+    let z3 = z^3
+        aiterms  = ixiter 0 1 $ \n t -> t*z3*((#)$3*n+1)/((#)$(3*n+1)*(3*n+2)*(3*n+3))
+        ai'terms = ixiter 0 z $ \n t -> t*z3*((#)$3*n+2)/((#)$(3*n+2)*(3*n+3)*(3*n+4))
+    in ai0 * (kahan_sum aiterms) + ai'0 * (kahan_sum ai'terms)
+
+bi0 = 3**(-1/6)/sf_gamma(2/3)
+bi'0 = 3**(1/6)/sf_gamma(1/3)
+airy_bi_series z =
+    let z3 = z^3
+        biterms  = ixiter 0 1 $ \n t -> t*z3*((#)$3*n+1)/((#)$(3*n+1)*(3*n+2)*(3*n+3))
+        bi'terms = ixiter 0 z $ \n t -> t*z3*((#)$3*n+2)/((#)$(3*n+2)*(3*n+3)*(3*n+4))
+    in bi0 * (kahan_sum biterms) + bi'0 * (kahan_sum bi'terms)
+
+main :: IO ()
+main = do
+    forM_ [-10,-9.99..5] $ \x -> do
+        let ax = sf_airy_ai x
+        let bx = sf_airy_bi x
+        putStr $ (show x)++" "++(show ax)++" "++(show bx)++"\n"
+
+----------------------------------------
 -- not perfect, but workable for now
 
 sf_erf :: Value -> Value
@@ -265,6 +293,13 @@ erfc_cf_pos1 z =
     let z2 = z^2
         as = z:[1/2,1..]
         bs = 0:cycle [z2,1]
+        cf = steeds as bs
+    in sf_exp(-z2) / (sqrt pi) * cf
+
+erfc_cf_pos2 z = 
+    let z2 = z^2
+        as = (2*z):(map (\n->(#)$ -(2*n+1)*(2*n+2)) [0..])
+        bs = 0:(map (\n->2*z2+(#)4*n+1) [0..])
         cf = steeds as bs
     in sf_exp(-z2) / (sqrt pi) * cf
 
