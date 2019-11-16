ae01a5a9004ec7b0cecee37f9f9fe9e45297bf5d
Apollo Hogan <apollo.hogan@gmail.com>
2019-11-15 20:22:44 -0500

    sech

--- Trig.hs
+++ Trig.hs
@@ -8,8 +8,6 @@ import Util
 sf_cos :: Value -> Value
 sf_cos x = kahan_sum $ ixiter 0 1 $ \n t -> -t*x^2/((#)$2*n+1)/((#)$2*n+2)
 
-sf_cosh = cosh
-
 -- vercosine function
 sf_vcos :: Value -> Value
 sf_vcos x = 2 * (sf_cos $ x/2)^2
@@ -23,8 +21,6 @@ sf_hvcos x = (sf_cos $ x/2)^2
 -- TODO: range reduce
 sf_sin :: Value -> Value
 sf_sin x = kahan_sum $ ixiter 1 x $ \n t -> -t*x^2/((#)$2*n)/((#)$2*n+1)
-
-sf_sinh = sinh
 
 sf_asin :: Value -> Value
 sf_asin = asin
@@ -48,13 +44,26 @@ sf_exsec x = 2 * (sf_sin $ x/2)^2 * (sf_sec x)
 -- tangent function
 sf_tan = tan
 
--- hyperbolic tangent function
-sf_tanh = tanh
+-- $$\tan z = z/(1- z^2/(3- z^2/(5- z^2/(7- ...))))$$
+-- NB terrible convergence
+tan_cf z = steeds (z:(cycle [-z^2,z^2])) (0:[1,3..])
 
 -- cotangent
 sf_cot x = sf_cos(x) / sf_sin(x)
 -- = 1/sf_tan x
 
+----------------------------------------
+-- hyperbolic cosine function
+sf_cosh = cosh
+
+-- hyperbolic sine function
+sf_sinh = sinh
+
+-- hyperbolic secant function
+sf_sech z = 1/sf_cosh z
+
+-- hyperbolic tangent function
+sf_tanh = tanh
 
 ----------------------------------------
 -- Gudermannian function
@@ -62,9 +71,4 @@ sf_gud x = sf_asin(sf_tanh x)
 -- = 2*sf_atan(sf_exp(x)) - pi/2
 -- = 2*sf_atan(sf_tanh(x/2))
 -- = sf_atan(sf_sinh(x))
-
--- $$\tan z = z/(1- z^2/(3- z^2/(5- z^2/(7- ...))))$$
--- NB terrible convergence
-tan_cf z = steeds (z:(cycle [-z^2,z^2])) (0:[1,3..])
-
 
